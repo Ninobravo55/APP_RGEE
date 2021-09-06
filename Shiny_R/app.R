6c84b78acd2e6b4d75fb7918a4386fcb1714091e
Ninobravo55 <geoambiental2030@gmail.com>
2021-09-06 07:10:25 -0500

    Proyecto en Rstudio

---
+++ Shiny_R/app.R
@@ -0,0 +1,215 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+# www.geomatica.pe
+# ----------------------------------------------------------------------
+
+# Descargar GIT y registrarse
+# Dirección para descargar Git: https://git-scm.com/downloads 
+# Dirección para registrar el usuario en GitHub: https://github.com
+
+# ---------------------------------------------------------------------
+# Configurar GIT su usuario y correo en el terminal
+#git config --global user.name "Ninobravo55"
+#git config --global user.email "geoambiental2030@gmail.com"
+
+# ---------------------------------------------------------------------
+
+# Activamos libreria GEE
+library(reticulate)
+library(rgee)
+library(googledrive)
+
+# App GEE
+library(shiny)
+library(lubridate)
+library(readxl)
+
+# ------------------------------------------------------------------------
+
+# Cargamos un archivo excel - lista provincia Peru - Dentro proyecto
+Lista_Provincia <- read_excel("Lista_Provincia_Excel.xlsx")
+
+# Seleccionamos los nombre provincia
+nom_prov = Lista_Provincia$NOMBPROV
+
+# ------------------------------------------------------------------------
+# Agregamos un featurecollection de GEE
+
+# Iniciar sesion RGEE
+
+ee_Initialize("bravomoralesnino@gmail.com", drive = T) # Activa drive
+
+
+
+
+# -----------------------------------------------------------------------
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+    
+    # Application title
+    titlePanel("Aplicativo en Google Earth Engine"),
+    
+    # Sidebar with a slider input for number of bins 
+    sidebarLayout(
+        sidebarPanel(
+            
+
+            # Seleccione satelite
+            selectInput(inputId = "Satelite_input",
+                        label = "Seleccione satelite",
+                        choices = c("Landsat_5"="LANDSAT/LT05/C01/T1_SR","Landsat_8"="LANDSAT/LC08/C01/T1_SR")),
+            
+            # Seleccionar rango de fecha
+            dateRangeInput("rango_fechas_input",
+                           h3("Rango de Fechas"),
+                           language = "es",
+                           format = "yyyy-mm-dd",
+                           start = "1900-01-01",
+                           end = today()-30,
+                           separator = " a "),
+            
+            # Seleccione el porcentaje de nubosidad
+            numericInput("nube_input","Ingrese porcentaje nubosidad",           
+                         min = 0,
+                         max = 100,
+                         step = 5,
+                         value = 20),
+            
+            # Seleccione provincia
+            selectInput(inputId="provincia_input",label="Seleccione Provincia",
+                        choices = nom_prov, selected = "LEONCIO PRADO",multiple = F),
+            
+            # Seleccione la composicion bandas
+            selectInput(inputId = "RGB_input",
+                        label = "Seleccione Composicion bandas",
+                        choices = c("B1"="B1","B2"="B2","B3"="B3","B4"="B4","B5"="B5","B6"="B6","B7"="B7")),
+            
+            # Boton de ejecutar cambio
+            submitButton("Ejecutar"),
+            
+            # Boton de salir
+            actionButton("salir_input","Salir"),
+            
+        ),
+        
+        # Show a plot of the generated distribution
+        mainPanel(
+            h3("Seleccion del satelite:"),
+            verbatimTextOutput("satelite_otput"),
+            h3("Seleccione rango fecha:"),
+            verbatimTextOutput("rango_fecha_otput"),
+            h3("Seleccion porcenaje nubosidad:"),
+            verbatimTextOutput("nubosidad_otput"),
+            h3("Seleccion de la provincia:"),
+            verbatimTextOutput("provincia_otput"),
+            h3("Seleccion composicion bandas:"),
+            verbatimTextOutput("RGB_otput"),
+            h3("Salida de informacion:"),
+            verbatimTextOutput("salida_otput"),
+            
+        )
+    )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+    
+    
+    # Seleccion imagen colleccion por satelite
+    output$satelite_otput <- renderPrint({
+        satelite <- input$Satelite_input
+        IC <- ee$ImageCollection(satelite)
+        
+    })
+    
+    
+    # Filtro de fecha de las imagenes collection
+    output$rango_fecha_otput <- renderPrint({
+        fecha_tex <- as.character(input$rango_fechas_input)
+        fecha_inicial <- fecha_tex[1]
+        fecha_final <- fecha_tex[2]
+
+        IC1 <- IC$filterDate(fecha_inicial, fecha_final)
+        
+    })
+    
+    # Filtro de porcentaje de nubosidad
+    output$nubosidad_otput <- renderPrint({
+        nube <- input$nube_input
+        IC2 <- IC1$filterMetadata('CLOUD_COVER','less_than',nube)
+    })
+    
+    # Filtro por zona de estudio
+    output$provincia_otput <- renderPrint({
+        
+        limite_provincia = ee$FeatureCollection('users/bravomoralesnino/Tabla/Limite_Provincia')
+        
+        Feature <- limite_provincia$filterMetadata("NOMBPROV", 'equals', input$provincia_input)
+        Map$centerObject(Feature,8)
+        
+        # Visualizar Feature
+        Map$addLayer(Feature, list(color = "00FF11"), input$provincia_input)
+        
+        
+        IC3 <- IC2$filterBounds(Feature)
+        
+        # Realizar la median
+        IC_mean <- IC3$median()
+        
+        # Recorte de la imagen
+        Img_mean_clip <- IC_mean$clip(Feature)
+        
+        # Conversion factor de escala
+        Img_mean_clip_es <- Img_mean_clip$multiply(0.0001)
+        
+        # Simbologia Landsat 5 y 7
+        viz_es <- list(bands = c('B5','B4','B3'),
+                       min = 0.1, 
+                       max = 0.6,
+                       gamma = 1.6)
+        
+        
+    })
+    
+    
+    
+    # Selecciona composicion de bandas
+    output$RGB_otput <- renderPrint({
+        input$RGB_input
+    })  
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    # Conversion factor de escala
+    #Img_mean_clip_es <- Img_mean_clip$multiply(0.0001)
+    
+    # Simbologia Landsat 5 y 7
+    #viz_es <- list(bands = c('B5','B4','B3'),
+    #               min = 0.1, 
+    #               max = 0.6,
+    #               gamma = 1.6)
+    
+    
+    
+    
+    # Salida del proyecto
+    output$salida_otput <- renderPrint({
+        input$salir_input
+    })
+    
+    
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
